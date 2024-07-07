417293ca325360696d1fe0c4db6baac5d7c605e8
Bigggggggg226 <115678872+Bigggggggg226@users.noreply.github.com>
2024-07-07 15:37:42 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -7,7 +7,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // https://www.uuidgenerator.net/
-let userID = '89b3cbba-e6ac-485a-9481-976a0415eab9';
+let userID = '6cd40d7d-6f4d-4902-afb4-895d82a0a21c';
 
 // https://www.nslookup.io/domains/cdn.xn--b6gac.eu.org/dns-records/
 // https://www.nslookup.io/domains/cdn-all.xn--b6gac.eu.org/dns-records/
